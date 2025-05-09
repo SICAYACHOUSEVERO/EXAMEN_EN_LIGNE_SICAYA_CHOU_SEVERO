9a625f8e3fbbefa27708e9bd3f3db21fe56a7dbf
SICAYACHOUSEVERO <severosicayachou20@gmail.com>
2025-05-09 01:15:41 +0100

    troisième commit : mises à jour des fichiers front-end et backend

--- index.js
+++ index.js
@@ -5,58 +5,139 @@ const connection = require('./db'); // Importamos la conexión de db.js
 const path = require('path');
 const bodyParser = require('body-parser');
 
+const multer = require('multer');
+const storage = multer.memoryStorage(); // o usar diskStorage para guardar archivos
+const upload = multer({ storage: storage });
+
+const session = require('express-session');
+
+app.use(session({
+  secret: 'claveSevero',  // cambia esto por una clave fuerte
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false }  // pon `true` solo si usas HTTPS
+}));
+
+
 app.use(express.urlencoded({ extended: true })); // ← necesario para manejar datos de formularios HTML
 app.use(bodyParser.json());
 app.use(express.static('public'));
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json()); // para JSON
 
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'views', 'login_register.html'));
 });
 
 app.get('/login', (req, res) => {
+    if (req.session.utilisateur) {
+       res.redirect('/profile');  
+    }
   res.sendFile(path.join(__dirname, 'views', 'login_register.html'));
 });
 app.get('/nouveau_examen', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'examen.html'));
 });
 
-app.post('/profile', (req, res) => {
-  const { email_login, password_login, tipe_login } = req.body;
+app.get('/profile', (req, res) => {
+  if (req.session.utilisateur) {
+    const query = "SELECT * FROM " + req.session.utilisateur.type + " WHERE id= ? ";
+    connection.query(query, [req.session.utilisateur.id], (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Erreur: problème avec la base de données');
+      }
+      const utilisateur = results[0];
+      res.render('profile', { utilisateur });
+    });
+  } 
+  else {
+ 
+    const { email_login, password_login, tipe_login } = req.body;
+    const isEnseignant = tipe_login !== undefined;
+    const tabla = isEnseignant ? 'enseignant' : 'etudiant';
 
-  const isEnseignant = req.body.tipe_login !== undefined; // Si el checkbox está marcado
-  var tabla= isEnseignant ? 'enseignant' : 'etudiant'
+    const query = "SELECT * FROM " + tabla + " WHERE mail = ? AND mot_de_passe = md5(?) ";
+    connection.query(query, [email_login, password_login], (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Erreur: problème avec la base de données');
+      }
+      if (results.length === 0) {
+        return res.status(401).send('Utilisateur non trouvé ou mot de passe incorrect');
+      }
 
-  const query = "SELECT * FROM "+tabla+" WHERE mail = ? AND mot_de_passe = md5(?) ";
-  connection.query(query, [email_login, password_login], (err, results) => {
-
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Erreur: problème avec la base de données');
-    }
-
-    if (results.length === 0) {
-      return res.status(401).send('Utilisateur non trouvé ou mot de passe incorrect');
-    }
 
       const utilisateur = results[0];
+      req.session.utilisateur = {id: utilisateur.id,  type: tabla };
 
-    res.render('profile', { utilisateur });
-  });
+      res.render('profile', { utilisateur });
+    });
+  }
 });
 
 
-app.post('/valider_examen', (req, res) => {
-  const { titre } = req.body;
-    res.send(titre);
+
+app.post('/valider_examen', upload.none(), (req, res) => {
 
 
+
+  const { titre, description, cible, numeroq, maxq } = req.body;
+  const numProfe=1; //a definir a traves de la sesion //restriccion a los usuarios
+
+  const sql = 'INSERT INTO examen VALUES (null, ?, ?, ?, 1)';
+  connection.query(sql, [titre, description, cible], (err, result) => {
+    if (err) console.error(err);
+    else{
+
+      var numeroqq=parseInt(numeroq);
+      var maxqq=parseInt(maxq);
+      for (let i = 1; i <= maxqq; i++) {
+        const question = req.body[`question${i}`];
+        if(question){
+          const [tipe, ennonce, cible, fichier, t_fichier, taux, duree] = 
+          ['tipe', 'ennonce', 'cible', 'fichier', 't_fichier', 'taux', 'duree'].map(key => req.body[`${key}${i}`]);
+          const sqlquestion='INSERT INTO question VALUES (null, ?, ?, ?, ?, ?, ?, ' + result.insertId + ')';
+
+          connection.query(sqlquestion, [tipe, ennonce, "Ruta", t_fichier, taux, duree], (err, resultq) => {
+            if (err) console.error(err);
+            else{
+              if(tipe=="Directe"){
+                  const reponse = req.body[`reponse${i}`];
+                  const sqlquestion="INSERT INTO reponse VALUES (null, ?, 'correct', " + resultq.insertId + ')';
+
+                  connection.query(sqlquestion, [ reponse ], (err, resultr) => {
+                    if (err) console.error(err);
+                    else console.log("Respuesta insertada con exito #"+ resultr.insertId); });
+
+              }else{
+                const maxReponses = parseInt(req.body[`reponses${i}`]);
+
+                for (let k = 1; k <= maxReponses; k++) {
+                  const reponse=req.body[`q${i}reponse${k}`];
+                  const valid = req.body[`q${i}cb${k}`] ? true : false;
+                  const validite= valid ? "correct" : "incorrect";
+
+                  const sqlquestion='INSERT INTO reponse VALUES (null, ?, ?, ' + resultq.insertId + ')';
+                  connection.query(sqlquestion, [reponse, validite], (err, resultr) => {
+                    if (err) console.error(err);
+                    else console.log("Respuesta insertada con exito #"+ resultr.insertId); });
+                }
+              }
+
+                console.log("Pregunta insertada con exito #"+ resultq.insertId);
+            }
+          });
+
+        }
+      }
+
+      res.send("examen insertado con exito con id = "+result.insertId);
+      }
+  });
 
 
   /*const { email_login, password_login, tipe_login } = req.body;
@@ -93,13 +174,16 @@ app.post('/inserer_utilisateur', (req, res) => {
     tabla= 'enseignant';
   }
 
-  const query = 'INSERT INTO '+tabla+' VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?)';
+  const query = 'INSERT INTO '+tabla+' VALUES (null, ?, ?, ?,md5(?), ?, ?, ?, ?)';
   connection.query(query, [nom, prenom, email, password, ddns, sexe, etablissement, filiere], (err, result) => {
-    if (err) {
+    if (err) 
       console.error(err);
-      return res.status(500).json({ message: 'Error al insertar' });
+    else {
+      req.session.utilisateur = {id:result.insertId,  type: tabla };
+
+       res.redirect('/profile');  
     }
-    res.json({ message: 'Insertado con éxito' });
+
   });
 
 });
--- public/JS/script.js
+++ public/JS/script.js
@@ -1,153 +1,92 @@
-
 function ajouter_reponse(id){
     numReponses=parseInt(document.getElementById('reponses'+id).value);
     document.getElementById('reponses'+id).value=(numReponses+1);
-    alert(document.getElementById('reponses'+id).value);
 
+    var reponse = document.createElement("input");      reponse.name = "q"+id+"reponse"+(numReponses+1);
+    reponse.className = "reponse_qcm";                  reponse.type = "text";
+    reponse.placeholder = "Tapez la reponse";           reponse.required = true; 
 
-    var reponse = document.createElement("input");
-    reponse.id = "q"+id+"reponse"+(numReponses+1);
-    reponse.name = "q"+id+"reponse"+(numReponses+1);
-    reponse.className = "reponse_qcm";
-    reponse.type = "text";
-    reponse.placeholder = "Tapez la reponse";
-    reponse.required = true; 
+    var nuevoCheckbox = document.createElement("input");    nuevoCheckbox.name = "q"+id+"cb"+(numReponses+1);
+    nuevoCheckbox.className = "checkbox";                   nuevoCheckbox.type = "checkbox";
 
-
-    var nuevoCheckbox = document.createElement("input");
-    nuevoCheckbox.id = "q"+id+"cb"+(numReponses+1);
-    nuevoCheckbox.name = "q"+id+"reponse"+(numReponses+1);
-    nuevoCheckbox.className = "checkbox";
-    nuevoCheckbox.type = "checkbox";
-
-    document.getElementById('question'+id).appendChild(nuevoReponse);
+    document.getElementById('question'+id).appendChild(reponse);
     document.getElementById('question'+id).appendChild(nuevoCheckbox);
     document.getElementById('question'+id).innerHTML += " correcte";
-
 }
 function ajouter_question(tipo){
-            numq=parseInt(document.getElementById('numeroq').value);
-            var espacios="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+    numq=parseInt(document.getElementById('numeroq').value);
+    var espacios="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+
+    var nuevoDiv = document.createElement("div");
+    nuevoDiv.id = "question"+(numq+1);      nuevoDiv.className = "inputbox2";
+
+    var h6=document.createElement("h6");
+    h6.innerHTML+="Question " +tipo+" : "+ espacios+ "Taux : ";
+
+    var inputQuestion=document.createElement("input");
+    inputQuestion.type="hidden";        inputQuestion.name="question"+(numq+1);   inputQuestion.value=(numq+1); 
+
+    var inputTipe=document.createElement("input");
+    inputTipe.type="hidden";        inputTipe.name="tipe"+(numq+1);   inputTipe.value=tipo;               
+
+    const label = document.createElement('label');
+    label.className = 'label_fichier';      label.setAttribute('for', 'fichier' + (numq + 1));
+    label.id = 'label_fichier' + (numq + 1);label.textContent = ' Choisir';
+
+    const inputFile = document.createElement('input');
+    inputFile.type = 'file';        inputFile.name = 'fichier' + (numq + 1);    inputFile.id = 'fichier' + (numq + 1);
+    inputFile.disabled = true;
+
+    var question = document.createElement("textarea");
+    question.id = "ennonce" +(numq+1);      question.name = "ennonce" +(numq+1);    question.className = "question";
+    question.type = "text";   question.placeholder = "Tapez l'énoncé de la question";question.required = true;
 
 
-            var nuevoDiv = document.createElement("div");
-            nuevoDiv.id = "question"+(numq+1);
-            nuevoDiv.name = "question";
-            nuevoDiv.className = "inputbox2";
+    var hijos="<input id='taux"+(numq+1)+"' name='taux"+(numq+1)+"' "+
+    "type='number' class='taux' value='10' max='100' min='2' step='100' />%"+espacios+"Duree : "+
 
-            var h6=document.createElement("h6");
-            h6.innerHTML+="Question " +tipo+" : "+ espacios+ "Taux : ";
+    "<input id='duree"+(numq+1)+"' name='duree"+(numq+1)+"' "+
+    "type='number' class='taux' value='60' max='1800' min='30' step='10' /> sec."+espacios+
 
-            var inputTaux=document.createElement("input");
-            inputTaux.type="number";
-            inputTaux.id = "taux"+(numq+1);
-            inputTaux.name= "taux"+(numq+1);
-            inputTaux.className= "taux";
-            inputTaux.value="10";
-            inputTaux.max="100";
-            inputTaux.min="2";
-            inputTaux.step="2";
+    "<select id='select"+(numq+1)+"' name='t_fichier"+(numq+1)+"' class='fichier' onchange='select_fichier("+(numq+1)+")' >"+
+        "<option value='none' selected> + Fichier</option>"+
+        "<option value='audio' >Audio</option>"+
+        "<option value='image' >Image</option>"+
+        "<option value='video' >Video</option></select>";
 
-            var inputDuree=document.createElement("input");
-            inputDuree.id = "duree"+(numq+1);
-            inputDuree.name= "duree"+(numq+1);
-            inputDuree.className= "taux";
-            inputDuree.type="number";
-            inputDuree.value="60";
-            inputDuree.max="1800";
-            inputDuree.min="30";
-            inputDuree.step="10";
+    var bSupprimer="<button class='button_supprimer' onclick='supprimer_question("+(numq+1)+")' >Supprimer</button>";
+            
+    var divR="";
+    if(tipo=="Directe"){
+        divR="<h6>Reponse</h6>" +
+       "<input class='reponse' type='text' name='reponse" +(numq+1) + "' " +
+       "placeholder='Tapez la reponse' required/>";
+    }else{
+        divR="<h6>Reponses : " + 
+        "<button type='button' class='button_ajouter' onclick='ajouter_reponse(" +(numq+1) + 
+        ")'>Reponse&nbsp;+</button> </h6>" +
 
-            var selectF=document.createElement("select");
-            selectF.id = "select"+(numq+1);
-            selectF.name= "select"+(numq+1);
-            selectF.className= "fichier";
-            selectF.type="number";
+        "<input type='hidden' name='reponses" +(numq+1) + "' id='reponses" +(numq+1) + "' value='2' />"+
 
-            const options = [
-                { value: 'none', text: '+ fichier', selected: true },
-                { value: 'audio', text: 'Audio' },
-                { value: 'image', text: 'Image' },
-                { value: 'video', text: 'Video' }
-            ];
+        "<input class='reponse_qcm' type='text' name='q" +(numq+1) + "reponse1' " +
+        "placeholder='Tapez la reponse' required />"+
+        "<input class='checkbox' type='checkbox' checked='true' name='q"+(numq+1)+"cb1'/> correcte"+
 
-        options.forEach(opt => {
-        const option = document.createElement('option');
-        option.value = opt.value;
-        option.textContent = opt.text;
-        if (opt.selected) option.selected = true;
-        selectF.appendChild(option);
-        });
-        selectF.onchange = function () {
-            select_fichier(numq + 1); // Call your function with the current number
-        };
-
-        const label = document.createElement('label');
-        label.className = 'label_fichier';
-        label.setAttribute('for', 'fichier' + (numq + 1));
-        label.id = 'label_fichier' + (numq + 1);
-        label.textContent = ' Choisir';
-
-        const inputFile = document.createElement('input');
-        inputFile.type = 'file';
-        inputFile.name = 'fichier' + (numq + 1);
-        inputFile.id = 'fichier' + (numq + 1);
-        inputFile.disabled = true;
-
-        const bSupprimer = document.createElement('button');
-        bSupprimer.type = 'button';
-        bSupprimer.className = 'button_supprimer';
-        bSupprimer.textContent = 'Supprimer';
-        bSupprimer.onclick = function () {
-            supprimer_question(numq + 1);
-        };
-
-        var question = document.createElement("textarea");
-        question.id = "ennonce" +(numq+1);
-        question.name = "ennonce" +(numq+1);
-        question.className = "question";
-        question.type = "text";
-        question.placeholder = "Tapez l'énoncé de la question";
-        question.required = true;
-
-
-            var divR="";
-           if(tipo=="Directe"){
-                divR="<h6>Reponse</h6>" +
-               "<input class='reponse' type='text' id='reponse" +(numq+1) + "' " +
-               "name='reponse" +(numq+1) + "' " +
-               "placeholder='Tapez la reponse' required/>";
-           }else{
-                divR="<h6>Reponses : " + 
-                "<button type='button' class='button_ajouter' onclick='ajouter_reponse(" +(numq+1) + 
-                ")'>Reponse&nbsp;+</button> </h6>" +
-
-                "<input type='hidden' name='reponses" +(numq+1) + "' id='reponses" +(numq+1) + "' value='2' />"+
-
-                "<input class='reponse_qcm' type='text' id='q" +(numq+1) + "reponse1' " +
-                "name='reponse" +(numq+1) + "' " +
-                "placeholder='Tapez la reponse' required />"+
-                "<input class='checkbox' type='checkbox' checked='true' "+
-                "id='q"+(numq+1)+"cb1'/> correcte"+
-
-                "<input class='reponse_qcm' type='text' id='q" +(numq+1) + "reponse2' " +
-                "name='reponse" +(numq+1) + "' " +
-                "placeholder='Tapez la reponse' required />"+
-                "<input class='checkbox' type='checkbox' "+
-                "id='q"+(numq+1)+"cb2'/> correcte";
-        }
+        "<input class='reponse_qcm' type='text' name='q" +(numq+1) + "reponse2' " +
+        "placeholder='Tapez la reponse' required />"+
+        "<input class='checkbox' type='checkbox' name='q"+(numq+1)+"cb2'/> correcte";
+    }
     
-            h6.appendChild(inputTaux);
-            h6.innerHTML+="%"+espacios+"Duree : ";
-            h6.appendChild(inputDuree);
-            h6.innerHTML+=" sec."+espacios;
-            h6.appendChild(selectF);
+            
+            h6.innerHTML+=hijos;
             h6.appendChild(label);
             h6.appendChild(inputFile);
-            h6.appendChild(bSupprimer);
-            h6.appendChild(question);
+            h6.innerHTML+=bSupprimer;
+            h6.appendChild(inputTipe);
 
             nuevoDiv.appendChild(h6);
+            nuevoDiv.appendChild(question);
+            nuevoDiv.appendChild(inputQuestion);
             nuevoDiv.innerHTML+=(divR);
             
 
@@ -197,20 +136,6 @@ function supprimer_question(num){
     document.getElementById("numeroq").value=numq-1;
     document.getElementById('nombreq').innerHTML="Questions #"+(numq-1);
     }
-
-document.getElementById('register_form').addEventListener('submit', async function(e) {
-      e.preventDefault();
-      const formData = new FormData(e.target);
-      const data = Object.fromEntries(formData.entries());
-      const res = await fetch('/inserer_utilisateur', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-      });
-
-      const result = await res.json();
-      alert(result.message);
-    });
 
 document.getElementById("switch-to-register").addEventListener('click', (event) =>{
     event.preventDefault();
